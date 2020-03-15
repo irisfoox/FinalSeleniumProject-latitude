083cbdfe9352910862eee323304ecfc7183450a7
Iris Foox <57198497+irisfoox@users.noreply.github.com>
2020-03-15 22:40:50 +0200

    Add files via upload

---
+++ TestData.java
@@ -0,0 +1,12 @@
+package first;
+
+import java.util.List;
+
+public interface TestData {
+	public static final String UrlTiberias="https://en.wikipedia.org/wiki/Tiberias";
+	public static final String UrlJerusalem="https://latitudelongitude.org/il/jerusalem/";
+	public static final String UrlHaifa="https://en.wikipedia.org/wiki/Haifa";
+	//public int getLatitudeDiff(String most,List<String>URLList,List<String>latList);
+	//public void latitudeToString(int difference, java.lang.String most, java.util.List<java.lang.String> URLList,
+			//java.util.List<java.lang.String> latList);
+}
