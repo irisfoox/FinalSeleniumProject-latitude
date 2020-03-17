2ba308cfdec3ddc48707f8df1ee4f6c08a09d35b
Iris Foox <57198497+irisfoox@users.noreply.github.com>
2020-03-17 17:00:23 +0200

    Update TestData.java

--- TestData.java
+++ TestData.java
@@ -6,7 +6,5 @@ public interface TestData {
 	public static final String UrlTiberias="https://en.wikipedia.org/wiki/Tiberias";
 	public static final String UrlJerusalem="https://latitudelongitude.org/il/jerusalem/";
 	public static final String UrlHaifa="https://en.wikipedia.org/wiki/Haifa";
-	//public int getLatitudeDiff(String most,List<String>URLList,List<String>latList);
-	//public void latitudeToString(int difference, java.lang.String most, java.util.List<java.lang.String> URLList,
-			//java.util.List<java.lang.String> latList);
+	
 }
